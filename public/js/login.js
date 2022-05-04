de99ab456f29a028a547cec9a2c1493fefb93e24
Evon Bausa <evon.bausa@gmail.com>
2022-05-04 14:52:36 -0700

    Send user data to server

--- public/js/login.js
+++ public/js/login.js
@@ -1,23 +1,21 @@
-function processLogin() {
+function processLogin(data) {
     alert('Your login was successful and totally secure!');
 }
 
 function postUserCredentials() {
-    $.ajax(
-        {
-            url: "http://localhost:3000/login",
-            type: "POST",
-            data: {
-                "email": $('#email-input').val(),
-                "password": $('#password-input').val() // hash this field later
-            },
-            success: processLogin
-        }
-    )
+    $.ajax({
+        url: "http://localhost:3000/postUserCredentials",
+        type: "POST",
+        data: {
+            "email": $('#email-input').val(),
+            "password": $('#password-input').val()
+        },
+        success: processLogin
+    })
 }
 
 function setup() {
-    $('#submit-credentials').click(postUserCredentials)
+    $('#submit-credentials').click(postUserCredentials);
 }
 
 $(document).ready(setup);
--- server.js
+++ server.js
@@ -1,16 +1,24 @@
 const express = require('express');
 const app = express();
 
-app.listen(process.env.PORT || 3000, function (err) {
+// You need this to be able to parse through the body of the post request
+const bodyparser = require("body-parser");
+app.use(bodyparser.urlencoded({
+  extended: true
+}));
+
+app.listen(process.env.PORT || 3000, (err) => {
     if (err) {
         console.log(err);        
     }
 
 })
-
-app.get('/login', (req, res) => {
+// Route for POST request for postUserCredentials
+app.post('/postUserCredentials', (req, res) => {
     console.log(`Your email is: ${req.body.email}`);
     console.log(`Your password is: ${req.body.password}`);
+    res.send();
 })
 
+// Instead of using app.get() for every file, just use express.static middleware and it serves all required files to client for you.
 app.use(express.static('./public'));
