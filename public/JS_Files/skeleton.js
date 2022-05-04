25b056703261ccbcd0f6fbf751da6d7f4fe84d4d
ALEXIE NARCISO <NA.LEXIE.AN@gmail.com>
2022-05-04 16:55:40 -0700

    added footer placholder

--- public/JS_Files/skeleton.js
+++ public/JS_Files/skeleton.js
@@ -1,5 +1,7 @@
 function loadSkeleton(){
     $('#navbarPlaceholder').load("../../public/SkeletonHTML/navBar.html");
+    $('#footerPlaceholder').load("../../public/SkeletonHTML/footer.html");
+
 }
 
 loadSkeleton(); 
