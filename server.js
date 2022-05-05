fa9dd541007d00527aad047c44c0c4c924f2048d
Nathan Ng <longcaster19@gmail.com>
2022-05-05 15:05:54 -0700

    Partially implement password checking with database

--- server.js
+++ server.js
@@ -2,10 +2,22 @@
 const express = require('express');
 const bodyparser = require("body-parser");
 const bcrypt = require('bcrypt');
+const mysql = require('mysql2');
+
+// Initiate express
 const app = express();
 
+// Connect client to database
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'yourpasswordhere',
+    database: 'sustainably',
+    multipleStatements: false
+})
 
-// You need this to be able to parse through the body of the post request
+
+// Parse through the body of the post request
 app.use(bodyparser.urlencoded({
     extended: true
 }));
@@ -20,6 +32,15 @@ app.listen(process.env.PORT || 3000, (err) => {
 // Route for POST request for postUserCredentials
 app.post('/loginWithUserCredentials', (req, res) => {
     console.log(`Your email is: ${req.body.email}`);
+
+    connection.query(`SELECT password FROM users WHERE email = ${req.body.email}`, (err, results, fields) => {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log(results);
+            console.log(fields);
+        }
+    });
 
     // Hash the inputted password and check if it matches with password from db. HARDCODED PASSWORD: password
     bcrypt.compare(req.body.password, '$2b$10$YTZ2K3QsVDmotIHV4sMAROPTsBgHu96ZmhYJJAGXzHahgrU8EUwx.', (err, result) => {
