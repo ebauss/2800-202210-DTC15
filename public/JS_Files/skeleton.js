9f07d6ecd4ddbe2ecb6b512be68f7d476f1babbe
JoushuaDelaCruz <joushuadelacruz@gmail.com>
2022-05-04 17:57:03 -0700

    Finished creating the navbar

--- public/JS_Files/skeleton.js
+++ public/JS_Files/skeleton.js
@@ -1,5 +1,7 @@
+// Showing NavBar, Footer and FooterNavBar throughout the page
 function loadSkeleton(){
     $('#navbarPlaceholder').load("../../public/SkeletonHTML/navBar.html");
+
 }
 
 loadSkeleton(); 
