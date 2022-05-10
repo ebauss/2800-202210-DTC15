b4157759aeba44a5ca608a81e3870f8412b5054a
JoushuaDelaCruz <joushuadelacruz@gmail.com>
2022-05-10 14:02:34 -0700

    Made some changes to navBar, template and skeleton.js

--- public/JS_Files/skeleton.js
+++ public/JS_Files/skeleton.js
@@ -1,12 +1,7 @@
 // Showing NavBar, Footer and FooterNavBar throughout the page
 function loadSkeleton(){
-<<<<<<< HEAD
-    $('#navbarPlaceholder').load("../SkeletonHTML/navBar.html");
-    $('#footerPlaceholder').load("../SkeletonHTML/footer.html");
-=======
     $('#navbarPlaceholder').load("./SkeletonHTML/navBar.html");
     $('#footerPlaceholder').load("./SkeletonHTML/footer.html");
->>>>>>> JL_FrontEnd
 }
 
 loadSkeleton(); 
