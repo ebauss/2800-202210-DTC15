25d6cbf8892ca9f65d7610203909076d33eb0eec
ALEXIE NARCISO <NA.LEXIE.AN@gmail.com>
2022-05-10 14:28:57 -0700

    fix path

--- public/JS_Files/skeleton.js
+++ public/JS_Files/skeleton.js
@@ -1,7 +1,7 @@
 // Showing NavBar, Footer and FooterNavBar throughout the page
 function loadSkeleton(){
     $('#navbarPlaceholder').load("./SkeletonHTML/navBar.html");
-    $('#footerPlaceholder').load("./public/SkeletonHTML/footer.html");
+    $('#footerPlaceholder').load("./SkeletonHTML/footer.html");
 }
 
 loadSkeleton(); 
