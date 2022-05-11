33d769499560766e907602b82a7797efea5178ae
JoushuaDelaCruz <joushuadelacruz@gmail.com>
2022-05-10 23:10:05 -0700

    Finished the css for earnings both web and mobile design

--- README.md
+++ README.md
@@ -29,14 +29,12 @@ Content of the project folder:
 ```
 
 ## Citation:
-<<<<<<< HEAD
 * The Logo_only.png is created from Canvas by one of our groupmates -> Alexie Narcissco
 * The template.html is from COMP 1800 with these adaptations made 
     * Removing the bootstrap import link
     * Changing the meta name and the scripts
     * Removing the firebase links and imports
-=======
+    
 The Logo_only.png is created from Canvas by one of our groupmates -> Alexie Narcissco
 The icons used in the floatingbar are all from 8icons
     https://icons8.com/?msclkid=e4238e95d0a311ec923acc92a9f01d52
->>>>>>> Joushua_floatingBar
---
+++ public/JS_Files/earn.js
@@ -0,0 +1,39 @@
+// Mock Data
+current = 75
+
+// goal -= current
+goal = 25
+
+function createChart(current_points, goal_points) {
+    new Chart("doughnut-rewards-chart", {
+        type: "doughnut",
+        data: {
+            labels: ["Your Current Points", "Goal Points"],
+            datasets: [{
+                backgroundColor: ["#228B22", "#DCDCDC"],
+                data: [current_points, goal_points]
+            }]
+        },
+        options: {
+            title: {
+                display: true,
+                text: "My Current Points"
+            }
+        }
+    })
+}
+
+
+function currentMonth(){
+    const month = new Date().toLocaleString("default", {
+        month: "long"})
+
+        document.getElementById("display-month").innerHTML = month
+}
+
+function setup(){
+    currentMonth()
+    createChart(current, goal)
+}
+
+$(document).ready(setup)
