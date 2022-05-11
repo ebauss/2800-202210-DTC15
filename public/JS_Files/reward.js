af5f4cbaacf7ad601624d6197a9e4eb7ec6f999c
ALEXIE NARCISO <NA.LEXIE.AN@gmail.com>
2022-05-11 14:22:03 -0700

    modifying population method for rewards.html

--- public/JS_Files/reward.js
+++ public/JS_Files/reward.js
@@ -1,28 +1,28 @@
 rewards = [
     {
         id: "rewards number 0001",
-        title:"Get this Discount Reward Title",
+        title:"Get this Discount Reward Title 1",
         author: "sponsor for this discount goes here",
-        photo: 'https://picsum.photos/200/200',
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus sed sagittis enim, sit amet pretium neque. Sed in tempus ante. Suspendisse quis sem vitae urna interdum consequat vel vitae nibh.",
+        photo: 'https://picsum.photos/id/237/200',
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus sed sagittis enim, sit amet pretium neque. Sed in tempus ante.",
         cost: 50
     },
     {
         id: "rewards number 0002",
-        title:"Another Discount Reward Title",
+        title:"Another Discount Reward Title2",
         author: "sponsor for this discount goes here too",
-        photo: 'https://picsum.photos/200/200',
+        photo: 'https://picsum.photos/id/238/200',
         description:
-        "Maecenas vel diam et erat gravida vulputate ut eget arcu. Pellentesque ultricies convallis tincidunt. Vivamus et accumsan dui, eu dignissim ex. Suspendisse eu sem tristique, sagittis ligula nec, sollicitudin dui.",
+        "Maecenas vel diam et erat gravida vulputate ut eget arcu. Pellentesque ultricies convallis tincidunt. Vivamus et accumsan dui, eu dignissim ex.",
         cost: 100
     },
     {
         id: "rewards number 0003",
         title:"Again Discount Reward Title",
         author: "sponsor for this discount goes here as well",
-        photo: 'https://picsum.photos/200/200',
+        photo: 'https://picsum.photos/id/239/200',
         description:
-        "Maecenas vel diam et erat gravida vulputate ut eget arcu. Pellentesque ultricies convallis tincidunt. Vivamus et accumsan dui, eu dignissim ex. Suspendisse eu sem tristique, sagittis ligula nec, sollicitudin dui.",
+        "Pellentesque ultricies convallis tincidunt. Vivamus et accumsan dui, eu dignissim ex. Suspendisse eu sem tristique, sagittis ligula nec, sollicitudin dui.",
         cost: 1500
     }
     
@@ -34,41 +34,35 @@ rewards = [
 cardTag = ""
 
 function LoadRewards() { 
-    // console.log("I am Rewards")
-    for (i = 0; i < 12; i++){
-        // console.log(rewards)
-        
-        //for id = "place category " 
-        //img is from database link
-        // cost is out points, 
-
+    //for id = "place category " 
+    //img is from database link
+    // cost is out points, 
+    rewards.map(data => {
         cardTag =
         `<br>
-        <a href="http://localhost:3000/main.html/${rewards[0].id}">
-        <div class="rewards-container" id="?">
+        <div class="rewards-container" id="?">      
         <div class="image-container">
-        <img src="https://picsum.photos/200/200">
+        <img src="${data.photo}">
         </div>
         <div class="rewards-title">
-        <h2>${rewards[0].title}</h2>
+        <small class="rewards-code">${data.id}</small><br>
+        <h2>${data.title}</h2>
         </div>
         <div class="rewards-info">
-        <p>${rewards[0].description}</p>
+        <p>${data.description}</p>
         </div>
         <div class="cost">
-        ${rewards[0].cost} points
+        ${data.cost} points
+        <button class="redeem-points">Reedeem</button>
         </div>
-        </div>
-        </a>`
-        
-    }
+        </div>`
 
-    $("#rewards").append(cardTag)
-
+        $("#rewards").append(cardTag)
+    })
 }
 
 function setup() {
-    LoadRewards();
+    LoadRewards();    
 }
 
 $(document).ready(setup)
