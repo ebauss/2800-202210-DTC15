3b79efacfa8d011a675fec811273cea97e59fd27
Evon Bausa <evon.bausa@gmail.com>
2022-05-11 16:00:22 -0700

    Append logout button to navbar

--- public/JS_Files/navBar.js
+++ public/JS_Files/navBar.js
@@ -11,3 +11,44 @@ document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", 
     hamburger.classList.remove("active")
     navMenu.classList.remove("active")
 }))
+
+function isUserLoggedIn() {
+    $.ajax({
+        url: "http://localhost:3000/loginStatus",
+        type: "GET",
+        success: updateNavbar
+    })
+}
+
+function updateNavbar(data) {
+    if (data.loggedIn) {
+        $('#login-button-container').hide();
+        $('#logout-button-container').show();
+    } else {
+        $('#login-button-container').show();
+        $('#logout-button-container').hide();
+    }
+}
+
+async function logoutUser() {
+    await $.ajax({
+        url: "http://localhost:3000/logout",
+        type: "GET",
+        success: processLogout
+    })
+
+    isUserLoggedIn();
+}
+
+function processLogout(data) {
+    if (data) {
+        alert("You have successfully logged out!");
+    }
+}
+
+function setup() {
+    isUserLoggedIn();
+    $('body').on('click', '#logout-button', logoutUser);
+};
+
+$(document).ready(setup);
