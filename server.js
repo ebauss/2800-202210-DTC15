819e8e32e7a61721f6651defd5b88984b36be465
ALEXIE NARCISO <NA.LEXIE.AN@gmail.com>
2022-05-16 15:56:09 -0700

    Change to my password in server.js

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.use(session({
 const connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: 'fUt4b4$4kur4',
+    password: 'My$3qu@l',
     database: 'sustainably',
     multipleStatements: false
 })
