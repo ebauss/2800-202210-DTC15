7137b2b7f4bc6aef6b774353061c0afb5206d3c8
Nathan Ng <longcaster19@gmail.com>
2022-05-18 13:23:24 -0700

    Implement clientside code for receipt verification

--- public/JS_Files/verification.js
+++ public/JS_Files/verification.js
@@ -22,12 +22,31 @@ function processSingleReceiptData(data) {
     $('#value').val(data[0].reward_points / 100);
 
     $('#message').val(data[0].notes);
+}
 
-    
+function processVerification(data) {
+    console.log(data);
+    alert("Record has been updated motherfucker");
+}
+
+function requestVerification() {
+    today = new Date();
+
+    $.ajax({
+        url: "http://localhost:3000/verifyReceipt",
+        type: "POST",
+        data: {
+            value: $('#value').val(data[0].reward_points / 100),
+            notes: $('#message').val(data[0].notes),
+            verified_date: today.toISOString()
+        },
+        success: processVerification
+    })
 }
 
 function setup() {
     getSingleReceiptData();
+    $('#verify').click(requestVerification);
 };
 
 $(document).ready(setup);
