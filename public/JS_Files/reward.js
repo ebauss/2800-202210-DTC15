432e0ca0a4269212ee5d120ce46e4a6eb4be12ec
Nathan Ng <longcaster19@gmail.com>
2022-05-18 16:06:12 -0700

    Implement clientside code for redeeming a reward

--- public/JS_Files/reward.js
+++ public/JS_Files/reward.js
@@ -4,20 +4,20 @@ function processRewardsList(data) {
     data.forEach((reward) => {
         let cardTag =
             `<br>
-        <div class="rewards-container" id="?">      
-        <div class="image-container">
-        <img src="${reward.photo}">
-        </div>
-        <div class="rewards-title">
-        <h2>${reward.company} - $${reward.value}</h2>
-        </div>
-        <div class="rewards-info">
-        <p>${reward.description}</p>
-        </div>
-        <div class="cost">
-        ${reward.points_cost} points
-        <button class="redeem-points">Redeem</button>
-        </div>
+        <div class="rewards-container" id="${reward.reward_id}">      
+            <div class="image-container">
+                <img src="${reward.photo}">
+            </div>
+            <div class="rewards-title">
+                <h2>${reward.company} - $${reward.value}</h2>
+            </div>
+            <div class="rewards-info">
+                <p>${reward.description}</p>
+            </div>
+            <div class="cost">
+                ${reward.points_cost} points
+                <button class="redeem-points" id="${reward.points_cost}">Redeem</button>
+            </div>
         </div>`
 
         $("#rewards").append(cardTag);
@@ -44,10 +44,32 @@ function makeUserPointsRequest() {
     })
 }
 
+function processRedeemRequest(data) {
+    if (data) {
+        alert("You have redeemed the reward successfully.");
+    }
+}
+
+function makeRedeemRequest() {
+    today = new Date();
+
+    $.ajax({
+        url: "http://localhost:3000/redeemReward",
+        type: "POST",
+        data: {
+            reward_id: parseInt($(this).parent().parent().attr("id")),
+            redeemed_date: today.toISOString().split("T")[0],
+            cost: parseInt($(this).attr("id"))
+        },
+        success: processRedeemRequest
+    })
+}
+
 function setup() {
     makeRewardsListRequest();
     makeUserPointsRequest();
-    // loadRewards();    
+    
+    $('body').on('click', '.redeem-points', makeRedeemRequest)
 }
 
 $(document).ready(setup)
