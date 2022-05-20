6e6bb5e730272c234caa7d2d871650fdd26a26b9
Nathan Ng <longcaster19@gmail.com>
2022-05-20 14:25:52 -0700

    Add comments to main.js

--- public/js-files/main.js
+++ public/js-files/main.js
@@ -140,10 +140,12 @@ function LoadNews() {
     })
 }
 
+// changes the page's greeting to welcome the user by name
 function displayUsername(data) {
     $('.Greetings').html(`Welcome, ${data[0].first_name}!`);
 }
 
+// requests the signed-in user's details (only interested in the first name)
 function requestUsername() {
     $.ajax({
         url: "http://localhost:3000/checkProfile",
