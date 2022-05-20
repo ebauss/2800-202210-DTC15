a31bd6f9afef5c5c4b371e3ab04fc2f0839472a3
Nathan Ng <longcaster19@gmail.com>
2022-05-20 15:28:56 -0700

    Rename JS files, navbar, and footer according to convention

--- public/js-files/skeleton.js
+++ public/js-files/skeleton.js
@@ -1,6 +1,6 @@
 // Showing Nav-Bar, Footer and FooterNav-Bar throughout the page
 function loadSkeleton(){
-    $('#nav-bar-placeholder').load("./skeletons/nav-bar.html");
+    $('#navbar-placeholder').load("./skeletons/navbar.html");
     $('#footer-placeholder').load("./skeletons/footer.html");
     $('#floating-bar-placeholder').load("./skeletons/floating-bar.html");
 }
