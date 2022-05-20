b61ffb39eb72b7db526bc8aba47ee36a3f6a91fb
Nathan Ng <longcaster19@gmail.com>
2022-05-20 14:28:11 -0700

    Add comments to notification.js

--- public/js-files/notification.js
+++ public/js-files/notification.js
@@ -173,10 +173,12 @@ earningsButton.addEventListener('click', () => {
     }
 })
 
+// changes the page's greeting to welcome the user by name
 function displayUsername(data) {
     $('.hero > h2').html(`Welcome, ${data[0].first_name}!`);
 }
 
+// requests the signed-in user's details (only interested in the first name)
 function requestUsername() {
     $.ajax({
         url: 'http://localhost:3000/checkProfile',
