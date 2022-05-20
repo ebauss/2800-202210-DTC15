5126f71dc4f982a135c666d9bd465ca6b0d82247
JoushuaDelaCruz <joushuadelacruz@gmail.com>
2022-05-20 15:59:50 -0700

    adjusted the gap for profile

--- public/js-files/earn.js
+++ public/js-files/earn.js
@@ -129,6 +129,8 @@ function createChart(currentPoints, goalPoints) {
     })
 }
 
+createChart(monthlyTotalPoints, monthlyGoalPoints)
+
 //populates the goal and current points container
 function processUserRewardsInfo(data) {
     console.log(data);
