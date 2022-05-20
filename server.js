939d49db58f0955357eb4b980939d67b3e77fa81
Evon Bausa <evon.bausa@gmail.com>
2022-05-20 10:10:02 -0700

    Test backend img hosting

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.use(session({
 const connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: 'My$3qu@l',
+    password: 'K}{=2-D^Pwp5bgr&',
     database: 'sustainably',
     multipleStatements: false
 })
